4196dba66763a1bdd8d101da3c7f378d97617ed2
Matthew Curtis <matthew@matthewbryancurtis.com>
2016-06-15 10:59:23 -0700

    Fix non-conforming name scheme bug

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,9 @@ var comic = {
 	last: 143,
 	current: null,
 	buyURL: "https://www.topatoco.com/merchant.mvc?Screen=PROD&Store_Code=TO&Product_Code=BF-COMIC-PRINTS&Category_Code=BF&Product_Attributes%5b1%5d:value="
-}
+};
 
+//Update current comic with the selected buton id
 comic.update = function (action) {
 	switch (action) {
 		case "FIRST":
@@ -22,14 +23,21 @@ comic.update = function (action) {
 		case "RANDOM":
 			this.current = Math.floor(Math.random() * (this.last - this.first + 1)) + this.first;
 			break;
-	} 
+	}
 
-	//Fix special cases, 8, 30
+	//Change comic
+	//Fix special cases 8, 12, 30 (non-comforming naming scheme)
+	if (this.current === 8 || this.current === 12 || this.current === 30){
+		$('#comic').attr("src", "./images/2-" + this.current + ".png");
+	} else {
+		$('#comic').attr("src", "http://www.buttercupfestival.com/2-" + this.current + ".png");
+	}
 
-	$('#comic').attr("src", "http://www.buttercupfestival.com/2-" + this.current + ".png");
+	//Change comic label and buy link
 	$("#comic-ID").text("Series 2 Comic " + this.current);
-	$("#buy").attr("href", this.buyURL + this.current)
+	$("#buy").attr("href", this.buyURL + this.current);
 
+	//Disable buttons for first and last comics
 	$('button').prop('disabled', false);
 	if (this.current === this.first) {
 		$('#FIRST, #PREVIOUS').prop('disabled', true);
@@ -39,11 +47,11 @@ comic.update = function (action) {
 };
 
 $(function () {
+	//Initialize page with latest comic
 	comic.update("LAST");
 
+	//Add handlers to the buttons
 	$('button').click(function () {
 		comic.update($(this).attr("id"));
 	});
 });
-
-

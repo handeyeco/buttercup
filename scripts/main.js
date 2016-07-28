b87f8751e4ed685b6a7a6eacf24c38aff91189c7
Matthew Curtis <matthew@matthewbryancurtis.com>
2016-07-28 11:35:29 -0700

    Add user feedback for image load

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,13 @@ var comic = {
 
 //Update current comic with the selected buton id
 comic.update = function (action) {
+	var spinner = $('.buttercup-spinner');
+	var comicImg = $('#comic');
+
+	// "Disable" comic and show spinner
+	$(comicImg).css('opacity', 0.1);
+	$(spinner).show();
+
 	switch (action) {
 		case "FIRST":
 			this.current = this.first;
@@ -26,12 +33,23 @@ comic.update = function (action) {
 	}
 
 	//Change comic
+	//Create an image element
+	var imgPreload = new Image();
+
+	//Assign src to image element
 	//Fix special cases 8, 12, 30 (non-comforming naming scheme)
 	if (this.current === 8 || this.current === 12 || this.current === 30){
-		$('#comic').attr("src", "images/2-" + this.current + ".png");
+		imgPreload.src = "images/2-" + this.current + ".png";
 	} else {
-		$('#comic').attr("src", "http://www.buttercupfestival.com/2-" + this.current + ".png");
+		imgPreload.src = "http://www.buttercupfestival.com/2-" + this.current + ".png";
 	}
+
+	//Drop comic onto page, "Enable" comic, and remove spinner
+	$(imgPreload).load(function () {
+		$(comicImg).attr('src', imgPreload.src);
+		$(comicImg).css('opacity', 1);
+		$(spinner).hide();
+	});
 
 	//Change comic label and buy link
 	$("#comic-ID").text("Series 2 Comic " + this.current);
